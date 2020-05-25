14a9b55f31e973d0f764393c32ccfe264a450751
Thiru <thiru@ntucenterprise.sg>
2020-05-25 21:23:56 +0800

    Testing Sentry

--- main.go
+++ main.go
@@ -4,10 +4,25 @@ import (
         "fmt"
         "log"
         "net/http"
-        "os"
+	"os"
+	"time"
+	"github.com/getsentry/sentry-go"
 )
 
 func main() {
+
+        // Sentry for Error Tracking
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn: "https://556c91843255483d90b134ef4db2f9b4@o397787.ingest.sentry.io/5252636",
+	})
+	if err != nil {
+		log.Fatalf("sentry.Init: %s", err)
+	}
+	// Flush buffered events before the program terminates.
+	defer sentry.Flush(2 * time.Second)
+
+	sentry.CaptureMessage("It works!")
+
         // register hello function to handle all requests
         mux := http.NewServeMux()
         mux.HandleFunc("/", hello)
